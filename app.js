313d26acc773ef07f4ce0b1de905acf37c671e49
jestebanDesarrollo <jestebandesarrollador@gmail.com>
2025-02-23 23:32:53 -0500

    Se crean las variables para trabajar ademas de la funcion de agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let inputNombre = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+let amigos = []
+
+function agregarAmigo() {
+    let nombre = inputNombre.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, Ingresa un nombre válido.");
+        return ;
+    }
+
+    amigos.push(nombre);
+
+    inputNombre.value = "";
+
+
+};
+
+function actualizarLista() {
+
+}
